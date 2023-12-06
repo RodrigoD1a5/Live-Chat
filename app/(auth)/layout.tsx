1a265dc804948b5daab44483ee086635095ddccc
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2023-12-06 13:28:06 -0300

    style: add responsive layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,11 @@
 import { Logo } from "../ui/logo";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <main className="flex h-screen w-full flex-col items-center justify-center gap-8">
             <Logo />
-            <div className="flex w-2/3 flex-col gap-8">{children}</div>
+            <div className="flex w-2/3 min-w-fit max-w-xs flex-col gap-8">
+                {children}
+            </div>
         </main>
     );
 }
--- app/ui/chat/list.tsx
+++ app/ui/chat/list.tsx
@@ -1,14 +1,12 @@
 "use client";
-
 import { ChatContext } from "@/app/lib/contexts/chat";
 import { useContext } from "react";
 import { ChatMessage } from "./message";
-
 export function ChatList() {
     const { messages } = useContext(ChatContext);
 
     return (
-        <ul className="flex flex-1 flex-col gap-6 overflow-y-scroll px-4 pb-8 pt-28">
+        <ul className="scrollbar-hide flex max-w-screen-md flex-1 flex-col gap-6 overflow-y-scroll px-4 pb-8 pt-28">
             {Array.from(messages).map((message) => {
                 return (
                     <li key={message.id}>
