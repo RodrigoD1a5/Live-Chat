88165411ccc022a3e86fe6008dc07c3b91020693
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2023-12-06 10:03:29 -0300

    feat: add passwords matching validation

--- app/lib/actions/signup.ts
+++ app/lib/actions/signup.ts
@@ -1,13 +1,18 @@
 "use server";
-
 import { ZodError, z } from "zod";
 import { login } from "./login";
 
-const signUpSchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6).max(16),
-});
+const signUpSchema = z
+    .object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6).max(16),
+        confirmPassword: z.string().min(6).max(16),
+    })
+    .refine(({ password, confirmPassword }) => password === confirmPassword, {
+        message: "Passwords don't match.",
+        path: ["confirmPassword"],
+    });
 
 export async function signup(_prevState: any, formData: FormData) {
     try {
@@ -15,6 +20,7 @@ export async function signup(_prevState: any, formData: FormData) {
             name: formData.get("name"),
             email: formData.get("email"),
             password: formData.get("password"),
+            confirmPassword: formData.get("confirmPassword"),
         });
 
         /* Replace axios API request here to create a new user */
@@ -22,7 +28,6 @@ export async function signup(_prevState: any, formData: FormData) {
     } catch (error) {
         if (error instanceof ZodError) {
             const errors = [];
-
             for (const key in error.format()) {
                 switch (key) {
                     case "name":
@@ -34,14 +39,15 @@ export async function signup(_prevState: any, formData: FormData) {
                     case "password":
                         errors.push("Senha inválida. Precisa ter entre 6 e 16 caracteres.");
                         break;
+                    case "confirmPassword":
+                        errors.push("Senhas precisam ser iguais.");
+                        break;
                 }
             }
 
             return { message: errors.join("\n") };
         }
-
         return { message: "Falha ao fazer cadastro." };
     }
-
     return login(null, formData);
 }
