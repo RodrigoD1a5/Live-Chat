0e2b44e99682970382acab7f06be5b3cc3edb2db
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2023-12-06 17:45:34 -0300

    feat: add api connection to actions

--- app/lib/actions/login.ts
+++ app/lib/actions/login.ts
@@ -2,37 +2,27 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { ZodError, z } from "zod";
-import { Session } from "../definitions";
+import { api } from "../api/axios";
+
 const loginSchema = z.object({
     email: z.string().email(),
     password: z.string().min(6).max(16),
 });
+
 export async function login(_prevState: any, formData: FormData) {
     try {
-        const data = loginSchema.parse({
+        const { email, password } = loginSchema.parse({
             email: formData.get("email"),
             password: formData.get("password"),
         });
 
-        /* Replace axios API request here to get credentials */
-        const session = await new Promise<Session>((resolve) =>
-            setTimeout(
-                () =>
-                    resolve({
-                        user: {
-                            id: data.email,
-                            name: data.email,
-                        },
-                        token: "token-1",
-                    }),
-                3000,
-            ),
-        );
+        const { data } = await api.post("/signin", { email, password });
 
-        cookies().set("session", JSON.stringify(session), {
+        cookies().set("session", JSON.stringify(data), {
             maxAge: 60 * 60 * 24, // one day
         });
     } catch (error) {
+        console.error(error);
         if (error instanceof ZodError) {
             return { message: "Email ou senha inválidos." };
         }
--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -2,15 +2,14 @@ export interface User {
     id: string;
     name: string;
     email: string;
-    avatar?: string;
 }
+
 export interface Message {
     id: string;
     user: Omit<User, "email">;
     content: string;
     createdAt: string;
 }
-
 export interface Session {
     user: {
         id: string;
