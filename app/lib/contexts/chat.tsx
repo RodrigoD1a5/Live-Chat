8c86fc7126ac759b01b8b6144d416f52f785c9a0
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2023-12-06 16:45:05 -0300

    feat: add send message event

--- app/lib/contexts/chat.tsx
+++ app/lib/contexts/chat.tsx
@@ -1,14 +1,60 @@
 "use client";
-
 import { createContext, useCallback, useEffect, useState } from "react";
 import { initializeSocket } from "../api/socket";
 import { Message } from "../definitions";
 
+const socket = initializeSocket();
+
 export type ChatContextValue = {
     messages: Set<Message>;
-    addMessage(message: Message): void;
+    sendMessage(draft: Omit<Message, "id">): void;
 };
+
 export const ChatContext = createContext<ChatContextValue>({
     messages: new Set(),
-    addMessage: () => { },
+    sendMessage: () => { },
 });
+
+export function ChatContextProvider({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    const [messages, setMessages] = useState<ChatContextValue["messages"]>(
+        new Set(),
+    );
+
+    const addMessageToLocalSet = useCallback(
+        (message: Message) => {
+            setMessages(new Set(messages.add(message)));
+        },
+        [setMessages, messages],
+    );
+
+    const sendMessage = useCallback<ChatContextValue["sendMessage"]>(
+        (draft) => {
+            socket.emit("message", JSON.stringify(draft));
+
+            const message = {
+                id: new Date().getTime().toString(),
+                ...draft,
+            };
+
+            addMessageToLocalSet(message);
+        },
+        [addMessageToLocalSet],
+    );
+
+    useEffect(() => {
+        socket.on("chat", (data) => {
+            const message = JSON.parse(data) as Message;
+            addMessageToLocalSet(message);
+        });
+    }, [addMessageToLocalSet]);
+
+    return (
+        <ChatContext.Provider value={{ messages, sendMessage }}>
+            {children}
+        </ChatContext.Provider>
+    );
+}
